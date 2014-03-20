a31278113493502e4a6b08b0f0d7740cd57f7605
Adam McKenney (AMM) <adam@quitetiny.com>
2014-03-20 13:37:11 -0400

    Fixed comments

--- Vex_Competition.c
+++ Vex_Competition.c
@@ -11,10 +11,9 @@
 void pre_auton()
 {
   // Set bStopTasksBetweenModes to false if you want to keep user created tasks running between
-  // Autonomous and Tele-Op modes. You will need to manage all user created tasks if set to false.
   bStopTasksBetweenModes = true;
 
-	// All activities that occur before the competition starts
+	// For all activities that occur before the competition starts
 	// Example: clearing encoders, setting servo positions, ...
 }
 
@@ -31,7 +30,7 @@ task autonomous()
 
 //************************ Driver Control ************************
 
-task usercontrol()
+task usercontrol() //Do This FIRST
 {
 
 	while (true)
